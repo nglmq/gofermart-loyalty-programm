b57733400b95e751981a5b07a208ade044e70bd7
mamko3b228 <am99270@gmail.com>
2024-06-01 22:25:54 +0300

    handlers for login, registration and load order

--- internal/http-server/server/server.go
+++ internal/http-server/server/server.go
@@ -5,7 +5,6 @@ import (
 	"github.com/nglmq/gofermart-loyalty-programm/internal/config"
 	"github.com/nglmq/gofermart-loyalty-programm/internal/http-server/handlers"
 	"github.com/nglmq/gofermart-loyalty-programm/internal/http-server/handlers/orders"
-	"github.com/nglmq/gofermart-loyalty-programm/internal/http-server/handlers/user-auth"
 	"github.com/nglmq/gofermart-loyalty-programm/internal/storage/postgres"
 	"log/slog"
 	"net/http"
@@ -24,7 +23,7 @@ func Start() (http.Handler, error) {
 
 	r.Route("/api/user/", func(r chi.Router) {
 		r.Post("/register/", handlers.RegistrationHandle(storage))
-		r.Post("/login/", user_auth.LoginHandle(storage))
+		r.Post("/login/", handlers.LoginHandle(storage))
 		r.Post("/orders/", orders.LoadOrderHandle(storage))
 	})
 
